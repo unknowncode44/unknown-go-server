c5f4031ee2595b7279217b74bdd15ac221a99525
HVDevs <80708560+unknowncode44@users.noreply.github.com>
2025-12-21 12:47:37 -0300

    Creada logica para crear un nuevo material

--- pkg/material/service.go
+++ pkg/material/service.go
@@ -1,19 +1,45 @@
 package material
 
 import (
-	"github.com/google/uuid"
+	"errors"
+
+	// "github.com/google/uuid"
 	"github.com/unknowncode44/unknown-go-server/pkg/entities"
 )
 
 // Service es una interfaz que permite a nuestro módulo API acceder al repositorio de Material
 type Service interface {
 	Create(material *entities.Material) (*entities.Material, error)
-	FindAll() ([]entities.Material, error)
-	FindByID(id uuid.UUID) (*entities.Material, error)
-	Update(material *entities.Material) (*entities.Material, error)
-	Deactivate(id uuid.UUID) error
+	// FindAll() ([]entities.Material, error)
+	// FindByID(id uuid.UUID) (*entities.Material, error)
+	// Update(material *entities.Material) (*entities.Material, error)
+	// Deactivate(id uuid.UUID) error
 }
 
 type service struct {
 	repo Repository
+}
+
+func NewService(repo Repository) Service {
+	return &service{repo: repo}
+}
+
+// Logica para crear nuevo material
+func (s *service) Create(material *entities.Material) (*entities.Material, error) {
+
+	// las validaciones se realizan en el servicio y no el handler
+	if material.Name == "" {
+		return nil, errors.New("Nombre del Material es requerido")
+	}
+	if material.Sector == "" {
+		return nil, errors.New("Rubro del Material es requerido")
+	}
+	if material.UnitOfMeasure == "" {
+		return nil, errors.New("Unidad de medida es requerido")
+	}
+
+	// Definimos al material como activo
+	material.IsActive = true
+
+	return s.repo.Create(material)
 }
