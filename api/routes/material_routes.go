f861f13a5be075d278dcfc506c47b8187a87cf64
unknowncode44 <80708560+unknowncode44@users.noreply.github.com>
2025-12-26 12:10:46 -0300

    Creada rutas en material router para los metodos creados en handlers.

---
+++ api/routes/material_routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/unknowncode44/unknown-go-server/api/handlers"
+)
+
+func MaterialRoutes(router fiber.Router, handler *handlers.MaterialHandler) {
+	materials := router.Group("/materials")
+
+	materials.Post("/", handler.Create)
+	materials.Get("/", handler.GetAll)
+	materials.Get("/:id", handler.GetById)
+}
