5de6417106065653bc66c32947ee98c7c10c9a3a
HVDevs <80708560+unknowncode44@users.noreply.github.com>
2025-12-28 00:58:19 -0300

    Mejoras de seguridad en metodo update. Mejoras de identacion de codigo para mejor lectura

--- pkg/material/repository.go
+++ pkg/material/repository.go
@@ -28,7 +28,8 @@ func NewRepo(db *gorm.DB) Repository {
 
 // Create inserta un nuevo registro de material en la base de datos.
 func (r *repository) Create(material *entities.Material) (*entities.Material, error) {
-	if err := r.db.Create(material).Error; err != nil {
+	if err := r.db.
+		Create(material).Error; err != nil {
 		return nil, err
 	}
 	return material, nil
@@ -37,7 +38,8 @@ func (r *repository) Create(material *entities.Material) (*entities.Material, er
 // FindAll recupera todos los registros de materiales.
 func (r *repository) FindAll() ([]entities.Material, error) {
 	var materials []entities.Material
-	if err := r.db.Find(&materials).Error; err != nil {
+	if err := r.db.
+		Find(&materials).Error; err != nil {
 		return nil, err
 	}
 	return materials, nil
@@ -46,7 +48,8 @@ func (r *repository) FindAll() ([]entities.Material, error) {
 // FindByID busca un material por su UUID.
 func (r *repository) FindByID(id uuid.UUID) (*entities.Material, error) {
 	var material entities.Material
-	if err := r.db.First(&material, "id = ?", id).Error; err != nil {
+	if err := r.db.
+		First(&material, "id = ?", id).Error; err != nil {
 		return nil, err
 	}
 	return &material, nil
@@ -54,7 +57,10 @@ func (r *repository) FindByID(id uuid.UUID) (*entities.Material, error) {
 
 // Update aplica los cambios de la entidad en la base y devuelve la entidad actualizada.
 func (r *repository) Update(material *entities.Material) (*entities.Material, error) {
-	if err := r.db.Model(material).Updates(material).Error; err != nil {
+	if err := r.db.
+		Model(&entities.Material{}).
+		Where("id = ?", material.ID).
+		Updates(material).Error; err != nil {
 		return nil, err
 	}
 	return material, nil
